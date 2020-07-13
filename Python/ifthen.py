8ce2924a1b56fd336ebf8799008059935cc3279f
Jon Wendorff <jwendorff@mail.greenriver.edu>
2020-07-12 22:48:57 -0700

    finalized ifthen script

--- Python/ifthen.py
+++ Python/ifthen.py
@@ -1,12 +1,16 @@
 #!/usr/bin/python3
 #if/then examples
-#
-
+#the elif funtion statement means else if 
+#you can also use just "else" which means anything other than the2 statmnts
 def main():
-  x=200
+  x=100
   y=100
   if x < y:
     print("x is less than y")
   elif x > y:
     print("x is greater than y")
+  else:
+    print("x is equal to y")
+
+
 main()

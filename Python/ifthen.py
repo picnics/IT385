41a6c786e3f90c9ee8aa6f5009364a22b74f86d4
Jon Wendorff <jwendorff@mail.greenriver.edu>
2020-07-12 22:40:18 -0700

    Added second conditional

--- Python/ifthen.py
+++ Python/ifthen.py
@@ -1,11 +1,12 @@
 #!/usr/bin/python3
 #if/then examples
-
+#
 
 def main():
-  x=10
+  x=200
   y=100
   if x < y:
     print("x is less than y")
-
+  elif x > y:
+    print("x is greater than y")
 main()

1d0bfb46b563a6a032502e02a16dc3a727c842ba
Jon Wendorff <jwendorff@mail.greenriver.edu>
2020-07-12 23:29:31 -0700

    added while loop

---
+++ Python/whileLoop.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+#example WHILE LOOP
+
+def runLoop():
+  i=0
+  while i < 10:
+    doWork()
+    i += 1
+
+
+def doWork():
+  print("All WORK and no PLAY makes JACK!! a DULL BOY")
+
+
+runLoop()

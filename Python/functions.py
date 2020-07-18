7a0a0da1119b094240bae838d2604d1ebe89e1ed
Jon Wendorff <jwendorff@mail.greenriver.edu>
2020-07-18 15:43:39 -0700

    cube function

--- Python/functions.py
+++ Python/functions.py
@@ -8,7 +8,9 @@ def func1():
 #function that takes an argument
 def addNums(val1,val2):
   print(val1+val2)
-
+#function that returns a value
+def cube(x):
+  return x * x * x
 
 
 #Call Functions 
@@ -16,5 +18,10 @@ def addNums(val1,val2):
 #print(func1())
 #print(func1)
 
-addNums(5, 6)
-addNums(val2 = 11, val1 = 9)
+#addNums(5, 6)# positional arguments
+#addNums(val2 = 11, val1 = 9)
+print(cube(5)) #value of the function that is being called 
+
+#named arguments 
+#the only condition is you must stay with one or the other 
+#example is you would have to keep doing postitional or named after you start 

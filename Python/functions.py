07b67afdc27d27fccfb92226ada291bd73f959aa
Jon Wendorff <jwendorff@mail.greenriver.edu>
2020-07-18 15:30:57 -0700

    Added addNums

--- Python/functions.py
+++ Python/functions.py
@@ -5,9 +5,16 @@
 def func1():
   print("something func-y")
 
+#function that takes an argument
+def addNums(val1,val2):
+  print(val1+val2)
+
 
 
 #Call Functions 
-func1()
-print(func1())
-print(func1)
+#func1()
+#print(func1())
+#print(func1)
+
+addNums(5, 6)
+addNums(val2 = 11, val1 = 9)

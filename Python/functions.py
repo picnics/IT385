a28b6a7c3ca8f5053db10e12a7fec1b9178ca1eb
Jon Wendorff <jwendorff@mail.greenriver.edu>
2020-07-18 15:51:56 -0700

    default vaulues

--- Python/functions.py
+++ Python/functions.py
@@ -11,7 +11,12 @@ def addNums(val1,val2):
 #function that returns a value
 def cube(x):
   return x * x * x
-
+#function with default value
+def power(num, power=2):#power inside parenthesis is squared 
+  result=1
+  for i in range (power):
+    result=result * num
+  print(result)
 
 #Call Functions 
 #func1()
@@ -20,7 +25,11 @@ def cube(x):
 
 #addNums(5, 6)# positional arguments
 #addNums(val2 = 11, val1 = 9)
-print(cube(5)) #value of the function that is being called 
+#print(cube(5)) #value of the function that is being called
+
+power(6,6)
+power(6,2)
+power(6)
 
 #named arguments 
 #the only condition is you must stay with one or the other 

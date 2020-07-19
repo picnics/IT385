e082e47e30bdeed89c67ae42063caa95ff34c9d1
Jon Wendorff <jwendorff@mail.greenriver.edu>
2020-07-18 18:23:25 -0700

    started Dictionary

---
+++ Python/dictExample.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+
+#create disctionary object
+#NOTE: we use curly braces when creating these
+
+eng2esp = {"one":"uno", "two":"dos", "three":"tres"   }#english2spanish,one i#s the key and uno is value
+
+
+print(eng2esp)
+
+#view elements
+print(eng2esp["one"])
+print(eng2esp["three"])
